805ef7ba7455d8ad16d4cbbde0b1ed2109aa2284
kpilszak <klaudia.pilszak@gmail.com>
2023-01-31 07:28:32 +0100

    Add moves to user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector('.grid');
 const blockWidth = 100;
 const blockHeight = 20;
+const boardWith = 560;
 
 const userStart = [230, 10];
 let currentPosition = userStart;
@@ -49,6 +50,29 @@ addBlocks();
 
 const user = document.createElement('div');
 user.classList.add('user');
-user.style.left = userStart[0] + 'px';
-user.style.bottom = userStart[1] + 'px';
+drawUser();
 grid.appendChild(user);
+
+function drawUser() {
+    user.style.left = userStart[0] + 'px';
+    user.style.bottom = userStart[1] + 'px';
+}
+
+function moveUser(e) {
+    switch(e.key) {
+        case 'ArrowLeft':
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 10;
+                drawUser();
+            }
+            break;
+        case 'ArrowRight':
+            if (currentPosition[0] < boardWith - blockWidth) {
+                currentPosition[0] += 10;                    
+                drawUser();
+            }
+            break;
+    }
+}
+
+document.addEventListener('keydown', moveUser);
